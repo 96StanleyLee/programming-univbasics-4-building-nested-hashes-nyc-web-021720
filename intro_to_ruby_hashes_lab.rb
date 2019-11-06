b52f869bfd5d8c0fc38d37e6c5cc39d51c4e1d0e
Stanley Lee <sachih@live.com>
2019-11-06 21:08:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,8 +36,8 @@ def monopoly_with_third_tier
 			names:{
 				reading_railroad: {},
 				pennsylvania_railroad: {},
-				key7: "",
-				key8: ""
+				b_and_o_railroad: {},
+				shortline_railroad: {}
 			}
 		}
 	}
